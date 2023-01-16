490b2db747692840cfae9efbe64151afd762aa7b
Woeful_Wolf <54476280+WoefulWolf@users.noreply.github.com>
2023-01-16 15:18:00 +0200

    New ocular project

--- ocular.cpp
+++ ocular.cpp
@@ -1,28 +1,82 @@
 #include "ocular.h"
+#include "detours/detours.h"
+#pragma comment(lib, "d3d11.lib")
 
-OcularHook Ocular::Init() {
-	bool hooked = false;
-	OcularHook oHook;
+namespace ocular {
+	void** swapChain_VTable;
+	void** device_VTable;
+	void** context_VTable;
 
-	while (!oHook.hooked) {
-		std::cout << "[Ocular] Initializing kiero..." << std::endl;
-		kiero::Status::Enum kiero_init = kiero::init(kiero::RenderType::D3D11);
-		if (kiero_init == kiero::Status::Success) {
-			switch (kiero::getRenderType())
-			{
-			case kiero::RenderType::D3D11:
-				oHook = D3D11Hook();
-				std::cout << "[Ocular] Kiero initialized using D3D11!" << std::endl;
-			}
+	std::unordered_map<VMT, void*> oMethods;
+
+	void Init() {
+		DXGI_SWAP_CHAIN_DESC sd;
+		ZeroMemory(&sd, sizeof(sd));
+		sd.BufferCount = 1;
+		sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		sd.OutputWindow = GetForegroundWindow();
+		sd.SampleDesc.Count = 1;
+		sd.SampleDesc.Quality = 0;
+		sd.Windowed = TRUE;
+		sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+		IDXGISwapChain* swap;
+		ID3D11Device* device;
+		ID3D11DeviceContext* context;
+
+		HRESULT res = S_FALSE;
+		while (res != S_OK) {
+			res = D3D11CreateDeviceAndSwapChain(
+				NULL,
+				D3D_DRIVER_TYPE_HARDWARE,
+				NULL,
+				0,
+				NULL,
+				0,
+				D3D11_SDK_VERSION,
+				&sd,
+				&swap,
+				&device,
+				nullptr,
+				&context
+			);
 		}
-		else {
-			std::cout << "[Ocular] Kiero failed to initialize: " << kiero_init << std::endl;
-		}
+
+		swapChain_VTable = *reinterpret_cast<void***>(swap);
+		device_VTable = *reinterpret_cast<void***>(device);
+		context_VTable = *reinterpret_cast<void***>(context);
 	}
 
-	return oHook;
+	void* ResolveFromMethod(VMT method) {
+		int methodVal = static_cast<int>(method);
+
+		// SwapChain
+		if (methodVal < 0x100)
+			return swapChain_VTable[methodVal];
+
+		// Device
+		if (methodVal < 0x200)
+			return device_VTable[methodVal - 0x100];
+
+		// Context
+		return context_VTable[methodVal - 0x200];
+	}
+
+	bool CreateHook(void* function, VMT method) {
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+
+		oMethods[method] = ResolveFromMethod(method);
+
+		LONG attachRes = DetourAttach(&oMethods[method], function);
+
+		LONG commitRes = DetourTransactionCommit();
+
+		if (attachRes == NO_ERROR && commitRes == NO_ERROR)
+			return true;
+		else
+			return false;
+	}
 }
 
-void Ocular::Shutdown() {
-	kiero::shutdown();
-}
--- ocular.h
+++ ocular.h
@@ -1,10 +1,51 @@
 #pragma once
-#include "kiero/kiero.h"
+#include <Windows.h>
+#include <d3d11.h>
+#include <unordered_map>
 
-#include "hooks/OcularHook.h"
-#include "hooks/D3D11Hook.h"
+namespace ocular {
+	namespace SwapChain {
+		typedef HRESULT(__stdcall* QUERYINTERFACE) (IDXGISwapChain* pSwapChain, REFIID riid, void** ppvObject);
+		typedef ULONG(__stdcall* ADDREF) (IDXGISwapChain* pSwapChain);
+		typedef ULONG(__stdcall* RELEASE) (IDXGISwapChain* pSwapChain);
+		typedef HRESULT(__stdcall* SETPRIVATEDATA) (IDXGISwapChain* pSwapChain, REFGUID Name, UINT DataSize, const void* pData);
+		typedef HRESULT(__stdcall* SETPRIVATEDATAINTERFACE) (IDXGISwapChain* pSwapChain, REFGUID Name, const IUnknown* pUnknown);
+		typedef HRESULT(__stdcall* GETPRIVATEDATA) (IDXGISwapChain* pSwapChain, REFGUID Name, UINT DataSize, void* pData);
+		typedef HRESULT(__stdcall* GETPARENT) (IDXGISwapChain* pSwapChain, REFIID riid, void** ppParent);
+		typedef HRESULT(__stdcall* GETDEVICE) (IDXGISwapChain* pSwapChain, REFIID riid, void** ppDevice);
+		typedef HRESULT(__stdcall* PRESENT) (IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
 
-namespace Ocular {
-	OcularHook Init();
-	void Shutdown();
+		typedef HRESULT(__stdcall* RESIZEBUFFERS) (IDXGISwapChain* pSwapChain, UINT BufferCount, UINT Width, UINT Height, DXGI_FORMAT NewFormat, UINT SwapChainFlags);
+	}
+
+	namespace Context {
+		typedef void(__stdcall* OMSETRENDERTARGETS) (ID3D11DeviceContext* pContext, UINT NumViews, ID3D11RenderTargetView* const* ppRenderTargetViews, ID3D11DepthStencilView* pDepthStencilView);
+		typedef void(__stdcall* CLEARDEPTHSTENCILVIEW) (ID3D11DeviceContext* pContext, ID3D11DepthStencilView* pDepthStencilView, UINT ClearFlags, FLOAT Depth, UINT8 Stencil);
+	}
+
+	enum class VMT {
+		// SwapChain
+		QueryInterface = 0,
+		AddRef = 1,
+		Release = 2,
+		SetPrivateData = 3,
+		SetPrivateDataInterface = 4,
+		GetPrivateData = 5,
+		GetParent = 6,
+		GetDevice = 7,
+		Present = 8,
+
+		ResizeBuffers = 13,
+
+		// Device
+
+		// Context
+		OMSetRenderTargets = 0x200 + 33,
+		ClearDepthStencilView = 0x200 + 53
+	};
+
+	extern std::unordered_map<VMT, void*> oMethods;
+
+	void Init();
+	bool CreateHook(void* function, VMT method);
 }
